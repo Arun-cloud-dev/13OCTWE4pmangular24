b040902e1a782025c87791687b4bd903af328d5a
Arun raj <riderarunraj@live.com>
2024-11-17 17:05:05 +0530

    update routes - router link - outlests

--- routing-app/src/app/app.routes.ts
+++ routing-app/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {FirstComponent} from './first/first.component';
 import {SecondComponent} from './second/second.component';
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 export const routes: Routes = [
   {path: 'first-component', component: FirstComponent },
   {path: 'second-component', component: SecondComponent },
   {path:'home', component:HomeComponent},
+  {path:'contact', component:ContactComponent ,}
 ];
