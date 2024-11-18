5127e8421ea0ba03719838e05f992e1d6cfcd1df
Arun raj <riderarunraj@live.com>
2024-11-18 18:36:09 +0530

    update

--- angular03/src/app/app.component.ts
+++ angular03/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
 
 
 
@@ -8,7 +9,7 @@ import { HeaderComponent } from './header/header.component';
   standalone:true,
   templateUrl:'./app.component.html',
   styleUrl:'./app.component.css',
-  imports:[HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
 
 })
 
